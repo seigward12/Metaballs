dbc079abc310e53d5816211d4f5da47d87363c05
Alexis Nicolas <alexisducanada@hotmail.com>
2024-02-21 14:52:18 -0500

    fixed project name used everywhere

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,12 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+int main() {
+    auto window = sf::RenderWindow{{1920u, 1080u}, "Metaballs"};
     window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
+    while (window.isOpen()) {
+        for (auto event = sf::Event{}; window.pollEvent(event);) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
