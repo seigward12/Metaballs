ba99daab8c9407eef71bf07f7498da31993a5d26
Alexis Nicolas <alexisducanada@hotmail.com>
2024-06-18 00:23:37 -0400

    added shader checking

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include "StateManager.hpp"
 
 int main() {
+	if (!sf::Shader::isAvailable()) {
+		return -1;
+	}
 	srand(time(NULL));
 	const unsigned int screenWidth = static_cast<unsigned int>(
 		sf::VideoMode::getDesktopMode().width * 0.75f);
