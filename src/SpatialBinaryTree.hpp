68968b3eb36af165995fc3a039d9301371c9c892
Alexis Nicolas <alexisducanada@hotmail.com>
2024-07-13 09:24:32 -0400

    added SpatialBinaryTyree

---
+++ src/SpatialBinaryTree.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <Particle.hpp>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+typedef std::map<float, const Particle*> ParticleMap;
+
+class SpatialBinaryTree {
+   public:
+	SpatialBinaryTree();
+	~SpatialBinaryTree();
+	bool emplace(const Particle*);
+	void erase(const Particle*);
+	void query(const sf::FloatRect&, std::unordered_set<Particle*>&);
+	void query(const Particle*, std::unordered_set<Particle*>&);
+	void move(const Particle*);
+
+   private:
+	ParticleMap bottomBoundTree;
+	ParticleMap leftBoundTree;
+	ParticleMap rightBoundTree;
+	ParticleMap topBoundTree;
+
+	// struct ParticlePositionInTrees {
+	// 	ParticleMap::iterator bottomIt;
+	// 	ParticleMap::iterator leftIt;
+	// 	ParticleMap::iterator rightIt;
+	// 	ParticleMap::iterator topIt;
+	// };
+	// std::unordered_map<const Particle*, ParticlePositionInTrees> leftBoundTree;
+};
